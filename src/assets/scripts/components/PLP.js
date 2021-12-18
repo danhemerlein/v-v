b1e089210411f47f503df65f28ece0a7d358c0a1
Dan Hemerlein <danhemerlein@gmail.com>
2021-12-17 23:06:34 -0500

    install react, some setup

---
+++ src/assets/scripts/components/PLP.js
@@ -0,0 +1,11 @@
+
+import React from 'react'
+
+const PLP = ({ }) => {
+  return (
+    <>
+    plp plp plp
+    </>
+  )
+}
+export default PLP;
--- src/assets/scripts/templates/collection.js
+++ src/assets/scripts/templates/collection.js
@@ -2,7 +2,32 @@ import '../theme';
 import '../../styles/templates/collection.scss';
 import getAllProducts from '../graphql/collection-starter-code';
 
+import ReactDOM from "react-dom"
+import React from "react"
+import  PLP  from "../components/PLP"
+
+
+
 
 // getAllProducts('test-collection') => returns a Promise, which resolves to an Array of Product Objects
 
 // Your Code Here
+
+
+const init = async () => {
+  const products = await getAllProducts('test-collection');
+  console.log(products)
+  return products;
+}
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  init()
+
+  const rootEl = document.getElementById("react-plp")
+
+  console.log(rootEl)
+
+  rootEl && ReactDOM.render(<PLP />, rootEl)
+
+});
