cc20b6cc888d6d18d842fa587b13b48fe2336143
Dan Hemerlein <danhemerlein@gmail.com>
2021-12-21 15:51:52 -0500

    keyboard accessibility for color swatches

--- src/assets/scripts/components/ColorSwatch.js
+++ src/assets/scripts/components/ColorSwatch.js
@@ -17,34 +17,55 @@ const ColorSwatch = ({ values, activeVariant, selectColor }) => {
     list-style: none;
   `;
 
-  const ColorLabel = styled.label`
-    display: inline-flex;
-    align-items: center;
-    cursor: pointer;
-    margin: ${remHelper[4]};
-
-    ${above.mobile`
-      margin: ${remHelper[8]};
-    `};
-  `;
-
-  const DefaultInput = styled.input`
-    opacity: 0;
-    width: 0;
-    height: 0;
-  `;
-
-  const CustomInput = styled.div`
-    position: relative;
+  const ListItem = styled.li`
     width: ${remHelper[12]};
     height: ${remHelper[12]};
-    border-radius: 100%;
-    background: ${({ color }) => COLORS[color]} no-repeat;
+    margin: ${remHelper[4]};
 
     ${above.mobile`
       width: ${remHelper[16]};
       height: ${remHelper[16]};
+      margin: ${remHelper[8]};
     `};
+  `;
+
+  const ColorLabel = styled.label`
+    display: inline-flex;
+    align-items: center;
+    cursor: pointer;
+    position: relative;
+    width: 100%;
+    height: 100%;
+
+    &:focus {
+      border: 2px solid ${COLORS.Black};
+    }
+  `;
+
+  const DefaultInput = styled.input`
+    appearance: none;
+    background-color: #fff;
+    margin: 0;
+    position: absolute;
+    width: 100%;
+    height: 100%;
+
+    z-index: 1;
+
+    &:focus {
+      outline: 1px solid black;
+      outline: 1px auto -webkit-focus-ring-color;
+    }
+  `;
+
+  const CustomInput = styled.div`
+    position: relative;
+    width: 100%;
+    height: 100%;
+    border-radius: 100%;
+    background: ${({ color }) => COLORS[color]} no-repeat;
+    position: absolute;
+    z-index: 2;
 
     ${({ isSelected, color }) =>
       isSelected &&
@@ -57,12 +78,14 @@ const ColorSwatch = ({ values, activeVariant, selectColor }) => {
     <SwatchContainer>
       {values.map((value) => {
         return (
-          <li key={value}>
+          <ListItem key={value}>
             <ColorLabel htmlFor={value} isSelected={activeColor === value}>
               <DefaultInput
                 type="radio"
                 name={value}
                 id={value}
+                data-value={value}
+                onChange={(e) => selectColor(e)}
                 defaultChecked={activeColor === value}
                 aria-checked={activeColor === value}
               />
@@ -74,7 +97,7 @@ const ColorSwatch = ({ values, activeVariant, selectColor }) => {
                 isSelected={activeColor === value}
               />
             </ColorLabel>
-          </li>
+          </ListItem>
         );
       })}
     </SwatchContainer>
